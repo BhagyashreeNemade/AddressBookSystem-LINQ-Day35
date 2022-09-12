0178f616d8b4295e4e0100a3933ec0a45f7c35bb
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-09-12 23:40:55 +0530

    Create Address Book DataTable

---
+++ AddressBookSystem/AddressBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class AddressBook
+    {
+        public DataTable GetAddressBook()
+        {
+            DataTable table = new DataTable();
+            return table;
+        }
+    }
+}
